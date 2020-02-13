89d44e2fa3e5dd6754c366223a7c4a449ae24aee
ty-tab <hyungje1@naver.com>
2020-02-13 18:13:02 +0900

    frame for windows

---
+++ src/graphic/WindowFrame.java
@@ -0,0 +1,30 @@
+package graphic;
+
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+
+public class WindowFrame extends Graphic {
+	private JFrame jf;
+	public WindowFrame(String name){
+		jf = new JFrame(name);
+		SpringLayout sl = new SpringLayout();
+
+		jf.setBounds(100, 100, Property.WIDTH, Property.HEIGHT);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.getContentPane().setLayout(sl);
+		sl.putConstraint(SpringLayout.NORTH, ws, 0, SpringLayout.NORTH, getContentPane());
+		sl.putConstraint(SpringLayout.WEST, ws, 0, SpringLayout.WEST, getContentPane());
+		sl.putConstraint(SpringLayout.SOUTH, ws, 0, SpringLayout.SOUTH, getContentPane());
+		sl.putConstraint(SpringLayout.EAST, ws, 0, SpringLayout.EAST, getContentPane());
+		jf.getContentPane().add(ws);
+
+		jf.setResizable(false);
+		jf.setLocationRelativeTo(null);
+		jf.setVisible(true);
+	}
+
+	public void render(int fps){
+		ws.render(fps);
+	}
+
+}
