cd67996f484be37c61470700cdcf312c3ca6de2b
ty-tab <hyungje1@naver.com>
2020-02-13 18:19:54 +0900

    linux frame implements graphic interface

--- src/graphic/LinuxFrame.java
+++ src/graphic/LinuxFrame.java
@@ -3,7 +3,7 @@ package graphic;
 // this is android(linux) based frame 
 // nothing special to do 
 
-public class LinuxFrame {
+public class LinuxFrame implements graphic{
 	
 	public LinuxFrame(){
 		ws = new WorkSpace();
