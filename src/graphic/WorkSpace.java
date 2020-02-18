4cfd656fe2e1b7ba10066551ec14a61b73d3e61d
ty-desktop <hyungje1@naver.com>
2020-02-19 00:56:21 +0900

    seperate keyboard handler

--- src/graphic/WorkSpace.java
+++ src/graphic/WorkSpace.java
@@ -14,8 +14,6 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -65,10 +63,11 @@ import util.Calc;
 import util.Rectangle;
 import window.NavigationBar;
 import window.Window;
+import eventhandler.KeyboardHandle;
 
 @SuppressWarnings("unchecked")
 
-public class WorkSpace extends Canvas implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener, Serializable, DropTargetListener {
+public class WorkSpace extends Canvas implements MouseListener, MouseMotionListener, MouseWheelListener, Serializable, DropTargetListener {
 	private static final long serialVersionUID = 3147910908867708447L;
 	private final String version = "190203";
 	DropTarget dt; // drag and drop
@@ -103,13 +102,15 @@ public class WorkSpace extends Canvas implements KeyListener, MouseListener, Mou
 	
 	//SubCircuit sc = new SubCircuit(7, 7);
 	
+	private KeyboardHandle kbHandle = new KeyboardHandle();
+	
 	public WorkSpace() {
 		super();
 		this.setBackground(Color.white);
 		this.setFocusable(true);
 		
 		addMouseListener(this);
-		addKeyListener(this);
+		addKeyListener(kbHandle);
 		addMouseMotionListener(this);
 		addMouseWheelListener(this);
 		dt = new DropTarget(this, DnDConstants.ACTION_COPY_OR_MOVE, this, true, null);
@@ -206,146 +207,7 @@ public class WorkSpace extends Canvas implements KeyListener, MouseListener, Mou
 		}
 	}
 	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		
-		for(Element ee : element) {
-			if(ee.contains(mx, my)) {
-				if(ee instanceof SubCircuitInput) {
-					SubCircuitInput sci = (SubCircuitInput) ee;
-					if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) sci.setID(sci.getID().substring(0, sci.getID().length()-1));
-					else if(e.getKeyCode() == KeyEvent.VK_SPACE) sci.setID(sci.getID() + " ");
-					else sci.setID(sci.getID() + KeyEvent.getKeyText(e.getKeyCode()));
-					return;
-				}
-				else if(ee instanceof SubCircuitOutput) {
-					SubCircuitOutput sco = (SubCircuitOutput) ee;
-					if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) sco.setID(sco.getID().substring(0, sco.getID().length()-1));
-					else if(e.getKeyCode() == KeyEvent.VK_SPACE) sco.setID(sco.getID() + " ");
-					else sco.setID(sco.getID() + KeyEvent.getKeyText(e.getKeyCode()));
-					return;
-				}
-				else if(ee instanceof TextBlock) {
-					TextBlock tb = (TextBlock) ee;
-					if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) tb.setText(tb.getText().substring(0, tb.getText().length()-1));
-					else if(e.getKeyCode() == KeyEvent.VK_SPACE) tb.setText(tb.getText() + " ");
-					else tb.setText(tb.getText() + KeyEvent.getKeyText(e.getKeyCode()));
-					return;
-				}
-			}
-		}
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
-			ratio *= 1.1;
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-			ratio *= 0.9;
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			for(Element ee : element) {
-				if(ee.contains(mx, my)) {
-					if(ee instanceof VPulse) {
-						VPulse vp = (VPulse) ee;
-						vp.setPulseWidth(vp.getPulseWidth() + 0.5);
-					}
-				}
-			}
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-			for(Element ee : element) {
-				if(ee.contains(mx, my)) {
-					if(ee instanceof VPulse) {
-						VPulse vp = (VPulse) ee;
-						vp.setPulseWidth(vp.getPulseWidth() - 0.5);
-					}
-				}
-			}
-		}
-		else if(e.isControlDown() == true) {
-			if(e.getKeyCode() == KeyEvent.VK_C) {
-				for(Element ee : element) {
-					if(ee.isSelected() == true) {
-						ctrlCV = ee;
-						return;
-					}
-				}
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_V) {
-				if(ctrlCV != null) {
-					Element copied = ctrlCV.copy();
-					copied.move(ctrlCV.getX() + 2, ctrlCV.getY() + 4);
-					ctrlCV = copied;
-					element.add(copied);
-				}
-			}
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_H) {
-			statusHide = !statusHide;
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_L) {
-			DigitalCircuitSimulator.FPS_LOCK = !DigitalCircuitSimulator.FPS_LOCK;
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_W) {
-			nodeExtension();
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-			sim();
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_P) {
-			for(Element ee : element) {
-				if(ee.contains(mx, my)) {
-					System.out.println(ee.toString());
-				}
-			}
-			for(Node nn : node) {
-				if(nn.contains(mx, my)) {
-					System.out.println(nn.toString());
-				}
-			}
-			for(GateIO io : ios) {
-				if(io.contains(mx, my)) {
-					System.out.println(io.toString());
-				}
-			}
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_C) {
-			for(Element ee : element) {
-				if(ee instanceof Switch) {
-					Switch ss = (Switch)ee;
-					if(ss.contains(mx, my)) ss.changeStatus();
-				}
-				else if(ee instanceof Vdc) {
-					Vdc vdc = (Vdc)ee;
-					if(vdc.contains(mx, my)) vdc.changeStatus();
-				}
-				else if(ee instanceof VPulse) {
-					VPulse vp = (VPulse) ee;
-					if(vp.contains(mx, my)) vp.setPulseWidth(1);
-				}
-			}
-		}
-		else if(e.getKeyCode() == KeyEvent.VK_DELETE) {
-			for(int i = 0 ; i < element.size() ; i++) {
-				if(element.get(i).isSelected()) {
-					element.get(i).removeData();
-					element.remove(i);
-					i--;
-				}
-			}
-			for(int i = 0 ; i < node.size(); i++) {
-				if(node.get(i).isSelected()) {
-					for(GateIO io : ios) {
-						if(io.isLinked() && io.getNode().equals(node.get(i))) {
-							io.setNode(null);
-						}
-					} 
-					node.remove(i);
-					i--;
-				}
-			}
-		}
-		
-		nb.keyPressed(e.getKeyCode());
-	}
+	
 
 	
 	@Override
@@ -755,12 +617,7 @@ public class WorkSpace extends Canvas implements KeyListener, MouseListener, Mou
 		oin.close();
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {	
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
+	
 	
 	private void nodeExtension() {
 		if(nodeMaking == true) {
